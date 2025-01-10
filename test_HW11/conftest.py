e2beb833616905fc5f71b4c4184b6396cec0a2ea
AnastasiyaMakarevich <itstart85@gmail.com>
2025-01-10 21:44:14 +0300

    add HW11

---
+++ test_HW11/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope="function")
+def driver():
+    options = Options()
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.implicitly_wait(5)
+    driver.maximize_window()
+    yield driver
+
+    driver.close()
+    driver.quit()
